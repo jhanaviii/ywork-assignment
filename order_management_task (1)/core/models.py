8ea0c2753ecb4e09da1d0358f5ab5c42ce1317ee
Jhanavi <126571652+jhanaviii@users.noreply.github.com>
2025-06-02 14:40:46 +0530

    Delete order_management_task (1) directory

--- order_management_task (1)/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Entry(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    user_email = models.EmailField()
-
-    def __str__(self):
-        return self.title
--- order_management_task (1)/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import EntryCreateAPIView, EntryListAPIView
-
-urlpatterns = [
-    path('add/', EntryCreateAPIView.as_view(), name='entry-add'),
-    path('fetch/', EntryListAPIView.as_view(), name='entry-fetch'),
-]
